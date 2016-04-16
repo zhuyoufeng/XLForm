e476bc5ebf7697d9a176e5e14ad2c5bc56337a7b
zhuyoufeng <zhuyoufeng1024@gmail.com>
2016-04-16 09:20:14 +0800

    Add cancel button for image cell.

--- XLForm/XL/Cell/XLFormImageCell.m
+++ XLForm/XL/Cell/XLFormImageCell.m
@@ -90,6 +90,11 @@
                                                           }]];
     }
     
+    
+    [alertController addAction:[UIAlertAction actionWithTitle: NSLocalizedString(@"BTN_CANCEL", nil)
+                                                        style: UIAlertActionStyleCancel
+                                                      handler: nil]];
+    
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
         alertController.modalPresentationStyle = UIModalPresentationPopover;
         alertController.popoverPresentationController.sourceView = self.contentView;
